b2fcc28ac9ee4308e1789fb2c1b7a8bc1bd83e10
Eivind Del Fierro <Eivind.Del.Fierro@gmail.com>
2023-06-07 18:31:40 -0400

    start

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // what is primary functionality = > pomodoro timer
+    // what are the features?
+      // user can set their own pomodoro time
+      // user can set their own rest time
+      // during rest time
+        // display palate cleanser
+
+
+        // have button to cancel rest time  
+        
+      // when rest time ends
+        // have button confirming rest time has ended
+          // button resets the clock to user configured rest time
+          // restarts clock immediately
+
+      // has options
+})
